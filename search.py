0089a97c1ac204058dcd96288b7031a41c9b9a12
suzanagi <realyutanemoto@gmail.com>
2019-11-17 23:14:22 +0900

    create tasks.md

--- search.py
+++ search.py
@@ -5,18 +5,32 @@ from yahoo_module import yahoo_search_module as yahoo
 from bing_module import bing_search_module as bing
 from baidu_module import baidu_search_module as baidu
 
-# Collect the search results from each search modules
+# Collect the search results from search modules
 def metasearch(query):
     results = list()
+    '''
     for item in google.search(query):
         results.append(item)
+    '''
     for item in yahoo.search(query):
         results.append(item)
     for item in bing.search(query):
         results.append(item)
+    '''
     for item in baidu.search(query):
         results.append(item)
+    '''
     return results
+
+def count(results):
+    resultList = dict()
+    for result in results:
+        if result.url in resultList:
+            print("multiple")
+            ++resultList[result.url]
+        else:
+            resultList[result.url] = 1
+    return resultList
 
 # Main Function
 if __name__ == "__main__":
@@ -27,7 +41,17 @@ if __name__ == "__main__":
         query = query + "+" + arg
     # Experiment the search function
     result = metasearch(query)
+
+    resultList = count(result)
+    
+    for key, value in resultList.items():
+        print(key)
+        print(value)
+        print("\n")
+
+    '''
     # Print the result list to the command line
     for item in result:
         print("[title] "+item.title)
         print("[url] "+item.url)
+    '''
